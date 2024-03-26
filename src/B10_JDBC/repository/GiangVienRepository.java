5fca7ab6e4c778444863da90f0143153439a70ab
HangNT169 <HangNT169@fpt.edu.vn>
2024-03-26 13:58:53 +0700

    Buổi 10: JDBC - Phần 1 - Giới thiệu + Phân tầng project

---
+++ src/B10_JDBC/repository/GiangVienRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package B10_JDBC.repository;
+
+/**
+ *
+ * @author hangnt
+ */
+public class GiangVienRepository {
+    // repository là tầng trao đổi trực tiếp 
+    // với CSDL 
+    // CRUD: 
+        // C: CREATE <=> INSERT INTO 
+        // R: READ <=> SELECT
+        // U: UPDATE <=> UPDATE 
+        // D: DELETE <=> DELETE 
+}
